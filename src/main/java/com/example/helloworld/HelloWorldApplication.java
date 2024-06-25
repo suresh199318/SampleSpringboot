99cfd778d9fddd36c59187234fa46223c4aba9a2
UpendraKakarla <164466592+UpendraKakarla@users.noreply.github.com>
2024-06-25 12:39:03 +0000

    adding comments

--- src/main/java/com/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/example/helloworld/HelloWorldApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HelloWorldApplication {
 
+//main method
     public static void main(String[] args) {
         SpringApplication.run(HelloWorldApplication.class, args);
     }
