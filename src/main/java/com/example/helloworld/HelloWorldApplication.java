6ba858b3344d6e3bf7c42ae2f16a226cdc563bec
Rahul Kausale <rahulkausale@gmail.com>
2024-12-03 10:26:23 +0000

    Adding unused method for SonarQube to generate a warning

--- src/main/java/com/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/example/helloworld/HelloWorldApplication.java
@@ -6,6 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HelloWorldApplication {
 
+    // Example of dead code that will generate a warning in SonarQube
+    private void unusedMethod() {
+        System.out.println("This method is never used!");
+    }
+    
     public static void main(String[] args) {
         SpringApplication.run(HelloWorldApplication.class, args);
     }
